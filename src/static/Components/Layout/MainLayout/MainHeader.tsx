f95b0e62793666c5093c276cc5bc62d0aafb0407
Suhyeon <heypitcher@hanmail.net>
2023-08-14 17:48:13 +0900

    홈로고 삽입

--- src/static/Components/Layout/MainLayout/MainHeader.tsx
+++ src/static/Components/Layout/MainLayout/MainHeader.tsx
@@ -1,3 +1,13 @@
-export const MainHeader = () => {
-  return <div></div>;
+import HomeRogo from "../../../../media/gosea-logo-light-blue.bbd279e7.png";
+import "../../../../CSS/MainLayout.css";
+
+export const MainHeader: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <img src={HomeRogo} className="HomeRogo" />
+      </div>
+      <div></div>
+    </div>
+  );
 };
