db6c9e195fcbebe2b9051935833bb44834f6d02a
Suhyeon <heypitcher@hanmail.net>
2023-08-14 18:07:51 +0900

    Link태그 추가

--- src/static/Components/Layout/MainLayout/MainHeader.tsx
+++ src/static/Components/Layout/MainLayout/MainHeader.tsx
@@ -1,5 +1,6 @@
 import HomeRogo from "../../../../media/gosea-logo-light-blue.bbd279e7.png";
 import "../../../../CSS/MainLayout.css";
+import { Link } from "react-router-dom";
 
 export const MainHeader: React.FC = () => {
   return (
@@ -7,7 +8,15 @@ export const MainHeader: React.FC = () => {
       <div>
         <img src={HomeRogo} className="HomeRogo" />
       </div>
-      <div></div>
+      <div>
+        <Link to="/Company">회사소개</Link>
+        <Link to="/Maintenance">정비상담</Link>
+        <Link to="/Businesstrip">출장정비</Link>
+        <Link to="/Mall">고씨몰</Link>
+        <Link to="/Community">커뮤니티</Link>
+        <Link to="/Faq">FAQ</Link>
+        <Link to="/SignIn">로그인</Link>
+      </div>
     </div>
   );
 };
