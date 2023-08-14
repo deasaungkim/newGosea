c007322bed68ddf2f29a2e03e091fc23b141b11e
Suhyeon <heypitcher@hanmail.net>
2023-08-14 17:33:37 +0900

    MainLayout.tsx에 MainHeader컴포넌트 연결

--- src/static/Components/Layout/MainLayout/MainLayout.tsx
+++ src/static/Components/Layout/MainLayout/MainLayout.tsx
@@ -1,9 +1,11 @@
 import "../../../../CSS/MainLayout.css";
 import { MainFooter } from "./MainFooter";
+import { MainHeader } from "./MainHeader";
 
 export const MainLayout = ({ children }: any) => {
   return (
     <div className="MainLayout">
+      <MainHeader />
       {children}
       <MainFooter />
     </div>
