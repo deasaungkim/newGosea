3d8727181827a26fecb713e1bfd3f65ae373e229
Suhyeon <heypitcher@hanmail.net>
2023-08-23 15:18:22 +0900

    CSS:(pc)메인 푸터 레이아웃

--- src/static/Components/Layout/MainLayout/MainFooter.tsx
+++ src/static/Components/Layout/MainLayout/MainFooter.tsx
@@ -5,29 +5,29 @@ import { Link } from "react-router-dom";
 export const MainFooter = () => {
   return (
     <div className="footer">
-      <div className="footer-service">
-        <div className="service-box">
-          <div className="service-head">서비스</div>
-          <div className="service-item">앱 다운로드</div>
-          <div className="service-item">자주 묻는 질문</div>
-        </div>
-        <div className="service-box">
-          <div className="service-head">회사</div>
-          <div className="service-item">고씨 채용</div>
-          <div className="service-item">제휴 문의</div>
-        </div>
+      {/* <div className="footer-service"> */}
+      <div className="service-box">
+        <div className="service-head">서비스</div>
+        <div className="service-item">앱 다운로드</div>
+        <div className="service-item">자주 묻는 질문</div>
       </div>
-      <div className="footer-copyright">
-        <div className="">
-          <img src={LOGO} alt="bottom-logo" className="footer-logo" />
-        </div>
-        <div className="bread">
-          <div>
-            <Link to="/" className="bread-item">
-              서비스 이용약관
-            </Link>
-          </div>
-        </div>
+      <div className="service-box">
+        <div className="service-head">회사</div>
+        <div className="service-item">고씨 채용</div>
+        <div className="service-item">제휴 문의</div>
+      </div>
+      {/* </div> */}
+      {/* <div className="footer-copyright"> */}
+      {/* <div className=""> */}
+      <img src={LOGO} alt="bottom-logo" className="footer-logo" />
+      {/* </div> */}
+      <div className="bread">
+        {/* <div> */}
+        <Link to="/" className="bread-item">
+          서비스 이용약관
+        </Link>
+        {/* </div> */}
+        {/* </div> */}
         <div className="copyright-text">
           <div>2021 &copy; 고씨 all rights reserved.</div>
           {/* <div>
