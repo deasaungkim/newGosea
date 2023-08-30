ce7996978b15199c30b608466ba49b54a1789b0e
Suhyeon <heypitcher@hanmail.net>
2023-08-30 18:28:48 +0900

    CSS:(PC)섹션 4,5

--- src/static/Routes/Landing/Main/MainPresenter.tsx
+++ src/static/Routes/Landing/Main/MainPresenter.tsx
@@ -1,9 +1,9 @@
 import "../../../../CSS/MainLayout.css";
 import section01 from "../../../../media/gosea-main-image-crop.b292ffb9.png";
-import section02 from "../../../../media/gosea-mian-section-001.f8792797.png";
-import section03 from "../../../../media/gosea-mian-section-002.7736c2b6.png";
-import section04 from "../../../../media/gosea-mian-section-003.d742f395.png";
-import section06 from "../../../../media/gosea-main-maintenance.6e891318.png";
+import section03 from "../../../../media/gosea-mian-section-001.f8792797.png";
+import section04 from "../../../../media/gosea-mian-section-002.7736c2b6.png";
+import section05 from "../../../../media/gosea-mian-section-003.d742f395.png";
+import section07 from "../../../../media/gosea-main-maintenance.6e891318.png";
 
 export const MainPresenter = () => {
   return (
@@ -28,14 +28,32 @@ export const MainPresenter = () => {
         </h3>
         <h3>전국에 있는 프로님들과 문제를 해결해 나갑니다.</h3>
       </div>
-      <div className="section03-container flex-row-justify-center">
-        <div className="section03-left flex-column-justify-center">
+      <div className="section-container">
+        <div className="section-title ">
           <h2>정보 확인부터</h2>
           <h2>결제까지 한번에</h2>
           <h3>하나의 플랫폼에서 페이지 이탈없이</h3>
           <h3>한번에 실시간 매칭을 신속성과 편리성 제공</h3>
         </div>
-        <img src={section02} className="section03-right" />
+        <img src={section03} className="section-img-right" />
+      </div>
+      <div className="section-container background-blou-02">
+        <img src={section04} className="section-img-left" />
+        <div className="section-title ">
+          <h2>실시간</h2>
+          <h2>시스템 도입</h2>
+          <h3>장비 가능 여부 및 요금 문의가 필요없이</h3>
+          <h3>장비내용 입력시 빅데이터로 자동으로 비용 계산 및 정산</h3>
+        </div>
+      </div>
+      <div className="section-container">
+        <div className="section-title">
+          <h2>실시간 1:1 채팅으로</h2>
+          <h2>맞춤형 정비</h2>
+          <h3>고객의 요구사항을 상세하게 듣기위해</h3>
+          <h3>정비 공급자 매칭 후 1:1채팅 진행 맞춤형 정비 가능</h3>
+        </div>
+        <img src={section05} className="section-img-right" />
       </div>
     </div>
   );
